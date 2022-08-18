f4e76db32b3df538a1939d2ec8fce9c012fa41dc
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-08-18 13:42:14 +0100

    Moved forecast to JS, linked to API
    + days name
    + min/max temp
    - icons

--- src/app.js
+++ src/app.js
@@ -30,6 +30,8 @@ function currentCityWeather(response) {
   humidity.innerHTML = `Humidity: ${Math.round(response.data.main.humidity)}%`;
   let wind = document.querySelector(".wind");
   wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+
+  getForecast(response.data.coord);
 }
 
 function showCurrentPosition(position) {
@@ -52,6 +54,49 @@ function changeCity(event) {
   event.preventDefault();
   let city = document.querySelector("#input-location").value;
   searchCityWeather(city);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "f8620effd7466dda0c3530a3be2c5f5e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastDaily = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecastDaily.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                <div class="card">
+                  <div class="card-body">
+                    <h2 class="day">${formatDay(forecastDay.dt)}</h2>
+                    <i class="fa-solid fa-cloud-showers-heavy"></i>
+                    <p class="temp-small">
+                      ${Math.round(
+                        forecastDay.temp.max
+                      )}° &zwnj; &zwnj; <span class="t-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
+                    </p>
+                  </div>
+                </div>
+              </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 navigator.geolocation.getCurrentPosition(showCurrentPosition);
